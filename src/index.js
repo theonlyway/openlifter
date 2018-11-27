78f95f40b408a3d997e3d197106a8dfd978c867e
Sean Stangl <sean.stangl@gmail.com>
2018-11-26 19:52:37 -0800

    Add a comment about what Provider is.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import configureStore from "./store";
-import "./index.css";
-import App from "./App";
+
 import * as serviceWorker from "./serviceWorker";
+import configureStore from "./store";
+import App from "./App";
+
+import "./index.css";
 
 ReactDOM.render(
+  // Provider is a React component from the react-redux library.
+  // Its purpose is to "provide" the given store to its child components.
+  // Because the Provider wraps the whole App here, the store is global state.
   <Provider store={configureStore()}>
     <App />
   </Provider>,
