056d34f30ce660aa4a692c6390274f8eae54b0c1
Sean Stangl <sean.stangl@gmail.com>
2019-01-21 10:52:48 -0800

    Move the ByDivision table display logic into its own Component.

--- src/components/results/ResultsView.js
+++ src/components/results/ResultsView.js
@@ -6,31 +6,16 @@
 
 import React from "react";
 import { connect } from "react-redux";
-import { FormControl, Panel, Table } from "react-bootstrap";
+import { FormControl, Panel } from "react-bootstrap";
 
-import { getAllResults } from "../../common/divisionPlace";
-import { getWeightClassStr } from "../../reducers/meetReducer";
-import {
-  getBest5SquatKg,
-  getBest5BenchKg,
-  getBest5DeadliftKg,
-  getFinalEventTotalKg
-} from "../../reducers/registrationReducer";
-
-import type { Category, CategoryResults } from "../../common/divisionPlace";
-import type { Entry } from "../../reducers/registrationReducer";
+import ByDivision from "./ByDivision";
 
 import styles from "./ResultsView.module.scss";
 
 const marginStyle = { margin: "0 20px 0 20px" };
 
 type Props = {
-  meetName: string,
-  formula: string,
-  lengthDays: number,
-  weightClassesKgMen: Array<number>,
-  weightClassesKgWomen: Array<number>,
-  entries: Array<Entry>
+  lengthDays: number
 };
 
 type State = {
@@ -71,78 +56,7 @@ class ResultsView extends React.Component<Props, State> {
     }
   };
 
-  renderEntryRow = (entry: Entry, category: Category, key: number): any => {
-    const classes = entry.sex === "M" ? this.props.weightClassesKgMen : this.props.weightClassesKgWomen;
-    const totalKg = getFinalEventTotalKg(entry, category.event);
-    const squatKg = getBest5SquatKg(entry);
-    const benchKg = getBest5BenchKg(entry);
-    const deadliftKg = getBest5DeadliftKg(entry);
-
-    // The place proceeds in order by key, except for DQ entries.
-    const place = totalKg === 0 ? "DQ" : key + 1;
-
-    return (
-      <tr key={key}>
-        <td>{place}</td>
-        <td>{entry.name}</td>
-        <td>{getWeightClassStr(classes, entry.bodyweightKg)}</td>
-        <td>{entry.bodyweightKg}</td>
-        <td>{entry.age}</td>
-        <td>{squatKg === 0 ? "" : squatKg}</td>
-        <td>{benchKg === 0 ? "" : benchKg}</td>
-        <td>{deadliftKg === 0 ? "" : deadliftKg}</td>
-        <td>{totalKg === 0 ? "" : totalKg}</td>
-        <td>TODO</td>
-      </tr>
-    );
-  };
-
-  renderCategoryResults = (results: CategoryResults, key: number): any => {
-    const { category, orderedEntries } = results;
-    const sex = category.sex === "M" ? "Men's" : "Women's";
-
-    // Gather rows.
-    let rows = [];
-    for (let i = 0; i < orderedEntries.length; i++) {
-      rows.push(this.renderEntryRow(orderedEntries[i], category, i));
-    }
-
-    return (
-      <Panel key={key}>
-        <Panel.Heading>
-          {sex} {category.weightClassStr}kg {category.equipment} {category.division} {category.event}
-        </Panel.Heading>
-        <Panel.Body>
-          <Table>
-            <thead>
-              <tr>
-                <th>Place</th>
-                <th>Name</th>
-                <th>Class</th>
-                <th>Bwt</th>
-                <th>Age</th>
-                <th>Squat</th>
-                <th>Bench</th>
-                <th>Deadlift</th>
-                <th>Total</th>
-                <th>Points</th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-          </Table>
-        </Panel.Body>
-      </Panel>
-    );
-  };
-
   render() {
-    const results = getAllResults(this.props.entries, this.props.weightClassesKgMen, this.props.weightClassesKgWomen);
-
-    let categoryPanels = [];
-    for (let i = 0; i < results.length; i++) {
-      categoryPanels.push(this.renderCategoryResults(results[i], i));
-    }
-
     return (
       <div style={marginStyle}>
         <Panel bsStyle="info">
@@ -159,7 +73,7 @@ class ResultsView extends React.Component<Props, State> {
           </Panel.Body>
         </Panel>
 
-        {categoryPanels}
+        <ByDivision />
       </div>
     );
   }
@@ -167,12 +81,7 @@ class ResultsView extends React.Component<Props, State> {
 
 const mapStateToProps = state => {
   return {
-    meetName: state.meet.name,
-    formula: state.meet.formula,
-    lengthDays: state.meet.lengthDays,
-    weightClassesKgMen: state.meet.weightClassesKgMen,
-    weightClassesKgWomen: state.meet.weightClassesKgWomen,
-    entries: state.registration.entries
+    lengthDays: state.meet.lengthDays
   };
 };
 
