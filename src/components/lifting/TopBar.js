7b7027c4752b747ff539c3a6a19a834fec856d19
Sean Stangl <sean.stangl@gmail.com>
2019-01-20 18:17:39 -0800

    Show the Divisions in the Lifting page TopBar. Closes #47
    Opted to not show the weight class because it's likely the audience will
    think that's the current loading weight.

--- src/components/lifting/TopBar.js
+++ src/components/lifting/TopBar.js
@@ -1,28 +1,44 @@
 // vim: set ts=2 sts=2 sw=2 et:
+// @flow
 //
 // The top bar of the Lifting page, containing huge text about the current lifter.
 
 import React from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
+import type { Entry, Registration } from "../../reducers/registrationReducer";
 
 import styles from "./TopBar.module.scss";
 
-class LiftingHeader extends React.Component {
+type Props = {
+  // ownProps from the LiftingView.
+  attemptOneIndexed: number,
+  orderedEntries: Array<Entry>,
+  currentEntryId: ?number,
+
+  // Props from Redux state.
+  registration: Registration
+};
+
+class LiftingHeader extends React.Component<Props> {
   render() {
     // Defaults, in case of no lifter.
     let lifterName = "Flight Complete";
+    let divisionsStr = null;
 
     // In the case of a lifter, set fields.
-    if (this.props.currentEntryId !== null) {
+    if (this.props.currentEntryId) {
       const idx = this.props.registration.lookup[this.props.currentEntryId];
       const entry = this.props.registration.entries[idx];
       lifterName = entry.name;
+
+      divisionsStr = " / " + entry.divisions.join(", ");
     }
 
     return (
       <div className={styles.topBar}>
         <span className={styles.lifterName}>{lifterName}</span>
+        <span className={styles.divisions}>{divisionsStr}</span>
       </div>
     );
   }
@@ -32,19 +48,6 @@ const mapStateToProps = state => {
   return {
     registration: state.registration
   };
-};
-
-LiftingHeader.propTypes = {
-  // Props calculated by the LiftingView.
-  attemptOneIndexed: PropTypes.number.isRequired,
-  orderedEntries: PropTypes.array.isRequired,
-  currentEntryId: PropTypes.number, // Can be null.
-
-  // Props passed from Redux state.
-  registration: PropTypes.shape({
-    entries: PropTypes.array.isRequired,
-    lookup: PropTypes.object.isRequired
-  }).isRequired
 };
 
 export default connect(
