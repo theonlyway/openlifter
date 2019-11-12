91df2efc2a89c2fee1665571d5db1b7e7b805ddb
Sean Stangl <sean.stangl@gmail.com>
2019-11-12 15:24:14 -0700

    Import the better Schwartz calculation from the OpenPowerlifting repo. Closes #233

--- src/logic/coefficients/schwartzmalone.ts
+++ src/logic/coefficients/schwartzmalone.ts
@@ -21,17 +21,66 @@
 
 import { Sex } from "../../types/dataTypes";
 
-// Calculated the Schwartz coefficient, used for men.
+// Calculates the Schwartz coefficient, used for men.
+//
+// The Schwartz formula replaced the Hoffinan formula.
+// Schwartz was proposed around February 1971.
+//
+// The exact formula was found in the magazine Powerlifting USA,
+// Vol.6, No.2, August 1982, on page 61. That text is reproduced below:
+//
+// Computerized Schwartz Formula...Dr. Lyle Schwartz has often been
+// asked for a means by which the formula he has given Powerlifting can be
+// programmed into a computer or a hand held calculator with sufficient
+// memory. To obtain the Schwartz Formula (SF) for bodyweights (BW) bet-
+// ween 40 and 126 kg, the expression is: SF = 0.631926 exp(+01) -
+// 0.262349 exp(+00) (BW) + 0.511550 exp(-02) (BW)^2 - 0.519738
+// exp(-04) (BW)^3 + 0.267626 exp(-06) (BW)^4 - 0.540132 exp(-09)
+// (BW)^5 - 0.728875 exp(-13) (BW)^6. For higher bodyweights, the follow-
+// ing simple formulae are used: for BW 126-136, SF = 0.5210-0.0012
+// (BW - 125), for BW 136-146, SF = 0.5090-0011 (BW - 135), for BW
+// 146-156, SF = 0.4980-0.0010 (BW - 145), and for BW 156-166, SF =
+// 0.4880-0.0090 (BW - 156)
+//
+// Schwartz is quoted as saying about the formula's development,
+//
+// "Since powerlifting was still a young sport in the early 1970s
+// there was uneven development in the three lifts on the part of most
+// self-trained athletes. I compensated for such unevenness by creating
+// artificial 'best' totals by adding together the current records in the
+// individual lifts. A 'best' total would have been achieved by that ideal
+// lifter who could match the best performances to date in all three
+// powerlifts. Then I fitted these data to an artificial curve and picked
+// off numbers from the curve."
 export const schwartz_coefficient = (bodyweightKg: number): number => {
-  // Values calculated by fitting to coefficient tables.
-  const A = 3565.902903983125;
-  const B = -2.244917050872728;
-  const C = 0.445775838479913;
+  const adjusted = Math.min(Math.max(bodyweightKg, 40.0), 166.0);
 
-  // Arbitrary choice of lower bound.
-  let adjusted = Math.max(bodyweightKg, 40.0);
-
-  return A * Math.pow(adjusted, B) + C;
+  if (adjusted <= 126.0) {
+    let x0 = 0.631926 * 10.0;
+    let x1 = 0.262349 * adjusted;
+    let x2 = 0.51155 * Math.pow(10.0, -2) * Math.pow(adjusted, 2);
+    let x3 = 0.519738 * Math.pow(10.0, -4) * Math.pow(adjusted, 3);
+    let x4 = 0.267626 * Math.pow(10.0, -6) * Math.pow(adjusted, 4);
+    let x5 = 0.540132 * Math.pow(10.0, -9) * Math.pow(adjusted, 5);
+    let x6 = 0.728875 * Math.pow(10.0, -13) * Math.pow(adjusted, 6);
+    return x0 - x1 + x2 - x3 + x4 - x5 - x6;
+  } else if (adjusted <= 136.0) {
+    return 0.521 - 0.0012 * (adjusted - 125.0);
+  } else if (adjusted <= 146.0) {
+    return 0.509 - 0.0011 * (adjusted - 135.0);
+  } else if (adjusted <= 156.0) {
+    return 0.498 - 0.001 * (adjusted - 145.0);
+  } else {
+    // The final formula as published for this piece does not match
+    // the coefficient tables.
+    //
+    // From the tables, the step is exactly 0.0004 per pound, which
+    // has been converted to kg below.
+    //
+    // For reference, the published original is:
+    //   0.4880 - 0.0090 * (adjusted - 156.0)
+    return 0.4879 - 0.00088185 * (adjusted - 155.0);
+  }
 };
 
 // Calculates the Malone coefficient, used for women.
