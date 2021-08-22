25d4dfcda5def08b28b10b1235d4c30526f04754
Sean Stangl <sean.stangl@gmail.com>
2021-08-21 19:52:56 -0600

    Figure out how to do a JSON data guard

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,27 @@
 //! The OpenLifter live scoreboard server.
 
+use openlifter_server::openlifter::*;
+
 #[macro_use]
 extern crate rocket;
+
+use rocket::serde::json::Json;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[get("/")]
-fn hello() -> &'static str {
+async fn hello() -> &'static str {
     "Hello, world!"
+}
+
+#[post("/", data = "<input>")]
+async fn update(input: Json<GlobalState>) -> &'static str {
+    dbg!(input);
+    "got it!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, update])
 }
