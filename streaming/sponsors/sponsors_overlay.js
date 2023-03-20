b519604e217ba94f37a26a7c9dc8918a49cdf4d2
Theonlyway <theonlyway@users.noreply.github.com>
2023-03-20 23:06:14 +1000

    image rotation

--- streaming/sponsors/sponsors_overlay.js
+++ streaming/sponsors/sponsors_overlay.js
@@ -1,2 +1,27 @@
 const urlParams = new URLSearchParams(window.location.search);
 const rotationTimeSeconds = parseInt(urlParams.get("rotation") || 15);
+const imageWidth = urlParams.get("image_width") || "350";
+const imageHeight = urlParams.get("image_height") || "200";
+const sponsorImages = [
+  "dental_members.jpg",
+  "electro_systems.jpg",
+  "ideal_nutrition.png",
+  "myo_release.png",
+  "wayv.png",
+];
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function rotateSponsors() {
+  while (true) {
+    for (const index in sponsorImages) {
+      let element = document.getElementById("rotateImage");
+      element.src = "images/" + sponsorImages[index];
+      element.height = imageHeight;
+      element.width = imageWidth;
+      await sleep(rotationTimeSeconds * 1000);
+    }
+  }
+}
