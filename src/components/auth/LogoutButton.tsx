7e0756175c88c748cd7f8ea28f5eb2649861c947
Theonlyway <theonlyway@users.noreply.github.com>
2023-07-08 00:50:36 +1000

    Buttons

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "react-bootstrap";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
-  return <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>;
+  return <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Button>;
 };
 
 export default LogoutButton;
